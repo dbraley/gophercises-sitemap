5e074f8c8029ff3a440e2ce8930135666ca535fb
David Braley <david.braley@gmail.com>
2019-07-22 15:44:14 -0400

    Get a web page

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a site map for")
 	flag.Parse()
 
-	fmt.Println(*urlFlag)
-
 	// GET the webpage
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+
 	// Parse all the links on the page
 	// build proper urls with our links
 	// filter out links w/ a different name
