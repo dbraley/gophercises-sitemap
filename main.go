85092a6e94cd7ebb8436e4a6171a3b1e525c1124
David Braley <david.braley@gmail.com>
2019-07-22 16:13:59 -0400

    Parsing and building links

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"flag"
-	"io"
+	"fmt"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+
+	link "github.com/dbraley/gophercises-link"
 )
 
 func main() {
-	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a site map for")
+	urlFlag := flag.String("url", "http://gophercises.com", "the url that you want to build a site map for")
 	flag.Parse()
 
 	// GET the webpage
@@ -17,9 +20,35 @@ func main() {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+
+	base := baseUrl.String()
+	fmt.Println(base)
 
 	// Parse all the links on the page
+	var hrefs []string
+	links, _ := link.Parse(resp.Body)
+
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		default:
+			fmt.Println("Ignoring " + l.Href)
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
+
 	// build proper urls with our links
 	// filter out links w/ a different name
 	// find all pages (BFS)
